c2a4a7e03620bd85e3839ae77272591d7afc536a
ArtemChubatyi <stgmdota@gmail.com>
2019-07-02 07:23:02 +0300

    Updates

---
+++ precache-manifest.c24c4cd8593e89d0bedf91f1747cb555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85879e2eb87a6009051de5af7eb19308",
+    "url": "/react-zgp/index.html"
+  },
+  {
+    "revision": "9478aaba780a8a6f2de4",
+    "url": "/react-zgp/static/css/main.a571baa6.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/react-zgp/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "9478aaba780a8a6f2de4",
+    "url": "/react-zgp/static/js/main.15531eaf.chunk.js"
+  },
+  {
+    "revision": "79ee11a08b5b4e6e0e8e",
+    "url": "/react-zgp/static/js/runtime~main.f30e12c4.js"
+  },
+  {
+    "revision": "d922498136b63518f59b8b0ee054b4fd",
+    "url": "/react-zgp/static/media/preloader.d9224981.gif"
+  },
+  {
+    "revision": "1175895b72cabd008cdbce0004cb20d0",
+    "url": "/react-zgp/static/media/video.1175895b.mp4"
+  }
+]);
